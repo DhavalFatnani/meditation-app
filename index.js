5dc77c1a7612a7dc31ab9e6a12a018f62438499c
Dhaval Fatnani <dhavalfatnani017@gmail.com>
2024-03-19 22:23:52 +0530

    Added much functionalities to it

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
+let play_button = document.querySelector(".play");
+let pause_button = document.querySelector(".pause");
+let bg_video = document.querySelector(".background-video");
+bg_video.pause();
+play_button.style.display = "inline";
+pause_button.style.display = "none";
+
 function togglePlayPause() {
-  let play_button = document.querySelector(".play");
-  let pause_button = document.querySelector(".pause");
+  let galaxy = document.querySelector(".galaxy");
+  let monk = document.querySelector(".monk");
+  let ocean = document.querySelector(".ocean");
+  let rain = document.querySelector(".rain");
 
-  let video = document.querySelector(".background-video");
-
-  if (video.paused) {
-    video.play();
+  if (bg_video.paused) {
+    bg_video.play();
     play_button.style.display = "none";
     pause_button.style.display = "inline";
-    reduceTime();
+    timerInterval = setInterval(Timer, 1000);
   } else {
-    video.pause();
+    bg_video.pause();
     pause_button.style.display = "none";
     play_button.style.display = "inline";
   }
@@ -18,12 +25,60 @@ function togglePlayPause() {
 
 function restartTimer() {}
 
-function setTimer(time) {
-  let displayTime = document.querySelector(".time");
-  let seconds = 0;
-  let timer = `${time}:${seconds}`;
+function Timer(time) {
+  let timeLimitInMinutes = Number(time);
+  let timeLimitInSeconds = timeLimitInMinutes * 60;
+  let timerElement = document.querySelector(".time");
+  let minutes = Math.floor(timeLimitInSeconds / 60);
+  let seconds = timeLimitInSeconds % 60;
 
-  displayTime.innerHTML = timer;
+  timeLimitInSeconds--;
+
+  if (timeLimitInSeconds < 0) {
+    timerElement.textContent = "00:00";
+    clearInterval(timerInterval);
+    return;
+  }
+
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+
+  timerElement.textContent = minutes + ":" + seconds;
 }
 
-function reduceTime() {}
+function triggerGalaxy() {
+  bg_video.src = "./images/galaxy.mp4";
+  if (bg_video.play) {
+    pause_button.style.display = "inline";
+    play_button.style.display = "none";
+  }
+}
+
+function triggerMonk() {
+  bg_video.src = "./images/monk.mp4";
+  if (bg_video.play) {
+    pause_button.style.display = "inline";
+    play_button.style.display = "none";
+  }
+}
+
+function triggerOcean() {
+  bg_video.src = "./images/ocean.mp4";
+  if (bg_video.play) {
+    pause_button.style.display = "inline";
+    play_button.style.display = "none";
+  }
+}
+
+function triggerRain() {
+  bg_video.src = "./images/rain.mp4";
+  if (bg_video.play) {
+    pause_button.style.display = "inline";
+    play_button.style.display = "none";
+  }
+}
