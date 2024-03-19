f8801f795ccd5822083c93bb5e49f352b157d2d7
Dhaval Fatnani <dhavalfatnani017@gmail.com>
2024-03-19 20:38:28 +0530

    moods buttons added

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
-function move() {
-  var elem = document.getElementById(".play");
-  var width = 1;
-  var id = setInterval(frame, 10);
-  function frame() {
-    if (width >= 100) {
-      clearInterval(id);
-    } else {
-      width++;
-      elem.style.width = width + "%";
-    }
+function togglePlayPause() {
+  let play_button = document.querySelector(".play");
+  let pause_button = document.querySelector(".pause");
+
+  let video = document.querySelector(".background-video");
+
+  if (video.paused) {
+    video.play();
+    play_button.style.display = "none";
+    pause_button.style.display = "inline";
+    reduceTime();
+  } else {
+    video.pause();
+    pause_button.style.display = "none";
+    play_button.style.display = "inline";
   }
 }
+
+function restartTimer() {}
+
+function setTimer(time) {
+  let displayTime = document.querySelector(".time");
+  let seconds = 0;
+  let timer = `${time}:${seconds}`;
+
+  displayTime.innerHTML = timer;
+}
+
+function reduceTime() {}
